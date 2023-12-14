58f5fc2a47dd414d6adefcd1cc05bee40795f6cf
yerayherreria <yerayherre@gmail.com>
2023-12-14 12:36:48 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -150,7 +150,7 @@ export default function Gallery() {
       </section>
     </div>
   );
-}*/
+}
 
 import { getImageUrl } from './utils.jsx';
 
@@ -177,3 +177,161 @@ export default function Profile({size}) {
     />
   );
 }
+
+function Item({ name, isPacked }) {
+  return (
+    <li className="item">
+      {name} {isPacked ? '✔' : '❌'}
+    </li>
+  );
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Lista de Empaque de Sally Ride</h1>
+      <ul>
+        <Item 
+          isPacked={true} 
+          name="Traje espacial" 
+        />
+        <Item 
+          isPacked={true} 
+          name="Casco con hoja dorada" 
+        />
+        <Item 
+          isPacked={false} 
+          name="Foto de Tam" 
+        />
+      </ul>
+    </section>
+  );
+}
+
+function Item({ name, importance }) {
+  return (
+    <li className="item">
+      {name} {importance>0 && <em>(Importancia: {importance})</em>}
+    </li>
+  );
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Lista de Empaque de Sally Ride</h1>
+      <ul>
+        <Item 
+          importance={9} 
+          name="Traje espacial" 
+        />
+        <Item 
+          importance={0} 
+          name="Casco con hoja dorada" 
+        />
+        <Item 
+          importance={6} 
+          name="Foto de Tam" 
+        />
+      </ul>
+    </section>
+  );
+}
+
+function Drink({ name }) {
+  let partOfPlant, caffeineContent, age;
+  name==="tea"?(partOfPlant = 'hoja') && (caffeineContent = '15–70 mg/taza') && (age = '4,000+ años'):(partOfPlant = 'grano') && (caffeineContent = '80–185 mg/taza') && (age = '1,000+ años');
+
+
+  return (
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Parte de la planta</dt>
+        <dd>{partOfPlant}</dd>
+        <dt>Contenido de cafeína</dt>
+        <dd>{caffeineContent}</dd>
+        <dt>Edad</dt>
+        <dd>{age}</dd>
+      </dl>
+    </section>
+  );
+}
+
+export default function DrinkList() {
+  return (
+    <div>
+      <Drink name="tea" />
+      <Drink name="coffee" />
+    </div>
+  );
+}*/
+
+let minion = [
+  {
+    name: "Kevin",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/kevin.jpg",
+    birth: "1951",
+    side:"de los buenos"
+  },
+  {
+    name: "Josua",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/Josua.jpg",
+    birth: "1672",
+    side:"malvado"
+  },
+  {
+    name: "Dave",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/dave.jpg",
+    birth: "1723",
+    side: "de los buenos"
+  },
+  {
+    name: "Mudito",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/mudito.jpeg",
+    birth: "1379",
+    side:"de los buenos"
+  },
+  {
+    name: "Llongueras",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/llongueras.jpg",
+    birth: "1687",
+    side: "malvado"
+  },
+  {
+    name: "Minioncé",
+    bio: "Le va el cante, dar la nota, ama los karaokes, es el rey y reina de la fiesta. Invítalo a tu fiesta o te arrepentirás.",
+    img: "assets/img/minionce.jpg",
+    birth: "1976",
+    side: "de los buenos"
+  },
+  {
+    name: "Lobeznion",
+    bio: "No lo enfades, este bichillo tiene muy malas pulgas...aunque sólo mide medio metro y ¡no puede ser más gracioso!",
+    img: "assets/img/lobeznion.jpg",
+    birth: "2017",
+    side: "malvado"
+  },
+  {
+    name: "Minion Presley",
+    bio: "Aquí debería haber una biografía de la vida de este minion, pero son gente muy misteriosa, así que tendrás que imaginártela...",
+    img: "assets/img/minion-presley.jpg",
+    birth: "2017",
+    side: "malvado"
+  }
+];
+minion.forEach(minion => {
+  /*<div class="card" style="width: 10rem;">
+            <img src={{minion.img}} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{minion.name}}</h5>
+              <p class="card-text">{{minion.bio}}</p>
+              <p class="card-text">{{minion.birth}}</p>
+            </div>
+        </div>*/
+});
